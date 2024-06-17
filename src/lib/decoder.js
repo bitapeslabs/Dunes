f480c407b32331832b79ba271bb1805bf745778a
encloinc <jelch2002@gmail.com>
2024-06-17 10:59:36 -0500

    feat(RAI-2/indexer.js,runeutils.js): Implemented new Runestone decoder from Apebrush, fixed some stuff on runeutils

--- src/lib/decoder.js
+++ src/lib/decoder.js
@@ -1,18 +1,16 @@
-require('dotenv').config({ path: '../../.env' });
-const { runestone } = require( '@runeapes/apeutils' );
-const { stripObject } = require('./helpers');
+require("dotenv").config({ path: "../../.env" });
+const { runestone } = require("@runeapes/apeutils");
+const { stripObject } = require("./helpers");
 
 const decipherRunestone = (txJson) => {
-
-    return {
-        
-
-        ...stripObject(runestone.decipher(txJson.hex) ?? {}),
-        cenotaph: !!txJson.vout.filter(utxo => utxo.scriptPubKey.asm.includes('OP_RETURN')).length
-    }
-
-}
+  return {
+    ...stripObject(runestone.decipher(txJson.hex) ?? {})?.Runestone,
+    cenotaph: !!txJson.vout.filter((utxo) =>
+      utxo.scriptPubKey.asm.includes("OP_RETURN")
+    ).length,
+  };
+};
 
 module.exports = {
-    decipherRunestone
-}
+  decipherRunestone,
+};
--- src/lib/indexer.js
+++ src/lib/indexer.js
@@ -1,43 +1,24 @@
 //Test libs
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 // ======
 
+const { fromBigInt, toBigInt } = require("./tools");
 
+const { Op } = require("sequelize");
 
-const {
-    fromBigInt,
-    toBigInt
-} = require('./tools')
+const { isMintOpen, getReservedName } = require("./runeutils");
 
-const { Op } = require('sequelize');
+const { SpacedRune, Rune: OrdRune } = require("@ordjs/runestone");
 
-const {
-    isMintOpen,
-    getReservedName
-} = require('./runeutils')
-
-const { SpacedRune, Rune: OrdRune } = require( '@ordjs/runestone' );
-
-
-
-
-require('dotenv').config({ path: '../../.env' })
-const { databaseConnection } = require('../database/createConnection')
-
-
-
-
+require("dotenv").config({ path: "../../.env" });
+const { databaseConnection } = require("../database/createConnection");
 
 //Conversions
 
-
-
 const getAccount = async (address, db) => {
-
-
-    //New runes minted from coinbase, or UTXO is not known
-    /*
+  //New runes minted from coinbase, or UTXO is not known
+  /*
 
         The indexer starts keeping track of Utxos at RUNE GENESIS block 840,000. Any utxo before this in the explorer will be seen as genesis for inputs.
 
@@ -45,31 +26,31 @@ const getAccount = async (address, db) => {
 
         Any edicts with Genesis as an input should be ignored.
     */
-    if(address === 'GENESIS'){ return {id: -1}; }
+  if (address === "GENESIS") {
+    return { id: -1 };
+  }
 
-    const {
-        Account
-    } = db;
+  const { Account } = db;
 
-    let account = await Account.findOne({where: {address: address}})
-    
-    if(account) { return account; }
+  let account = await Account.findOne({ where: { address: address } });
 
-    account = await Account.create({address: address}, {
-        address: address,
-        utxo_list: '[]'
-    })
+  if (account) {
+    return account;
+  }
 
-    return account
-}
+  account = await Account.create(
+    { address: address },
+    {
+      address: address,
+      utxo_list: "[]",
+    }
+  );
 
-const createTransaction = async (
-    Transaction,
-    Account, 
-    db
-) => {
+  return account;
+};
 
-    /*
+const createTransaction = async (Transaction, Account, db) => {
+  /*
 
     Rune Balances:
     {
@@ -78,53 +59,44 @@ const createTransaction = async (
     }
     */
 
-    const {
-        Transactions
-    } = db;
+  const { Transactions } = db;
 
-    const {
-        runestone,
-        hash,
-        block,
-        hex,
-        vout,
-        txIndex
-    } = Transaction;
-    
-    let transaction = await Transactions.findOne({where: { hash: Transaction.hash}})
-    
-    if(transaction) { return transaction };
+  const { runestone, hash, block, hex, vout, txIndex } = Transaction;
 
-    let NewTransactionData = {
-        block_id: block,
-        tx_index: txIndex,
-        address_id: Account.id,
-        value_sats: (
-            //Get all vout values and convert them to a big integer string
-            vout.map(vout => toBigInt(vout.value.toString(), 8))
+  let transaction = await Transactions.findOne({
+    where: { hash: Transaction.hash },
+  });
 
-            //Add big ints of all items to get sum
-            .reduce((a, b) => BigInt(a) + BigInt(b), BigInt(0))
-            
-            //Convert back to string
-            .toString()
-        ),
-        hex,
-        runestone: JSON.stringify(runestone),
-        hash: hash
-    }
+  if (transaction) {
+    return transaction;
+  }
 
-    transaction = await Transactions.create(NewTransactionData)
+  let NewTransactionData = {
+    block_id: block,
+    tx_index: txIndex,
+    address_id: Account.id,
+    value_sats:
+      //Get all vout values and convert them to a big integer string
+      vout
+        .map((vout) => toBigInt(vout.value.toString(), 8))
 
-    return transaction
-}
+        //Add big ints of all items to get sum
+        .reduce((a, b) => BigInt(a) + BigInt(b), BigInt(0))
 
+        //Convert back to string
+        .toString(),
+    hex,
+    runestone: JSON.stringify(runestone),
+    hash: hash,
+  };
 
+  transaction = await Transactions.create(NewTransactionData);
 
+  return transaction;
+};
 
 const getRuneAllocationsFromUtxos = (InputUtxos) => {
-
-    /*
+  /*
         Important: Rune Balances from this function are returned in big ints in the following format
         {
             [rune_protocol_id]: BigInt(amount)
@@ -134,102 +106,98 @@ const getRuneAllocationsFromUtxos = (InputUtxos) => {
         different from rune_id which is used by the DB for indexing.
     */
 
-    return InputUtxos
+  return (
+    InputUtxos
 
-    //Get allocated runes and store them in an array
-    .reduce((acc, utxo) => {
-        let RuneBalances = JSON.parse(utxo.rune_balances)
+      //Get allocated runes and store them in an array
+      .reduce((acc, utxo) => {
+        let RuneBalances = JSON.parse(utxo.rune_balances);
 
-        //Sum up all Rune balances for each input UTXO 
-        RuneBalances.forEach(rune => {
-            if(!acc[rune.rune_protocol_id]){ acc[rune.rune_protocol_id] = BigInt('0'); }
-            acc[rune.rune_protocol_id] += BigInt(rune.amount)
-        })
+        //Sum up all Rune balances for each input UTXO
+        RuneBalances.forEach((rune) => {
+          if (!acc[rune.rune_protocol_id]) {
+            acc[rune.rune_protocol_id] = BigInt("0");
+          }
+          acc[rune.rune_protocol_id] += BigInt(rune.amount);
+        });
 
-        return acc
-    }, {})
-}
+        return acc;
+      }, {})
+  );
+};
 
 const getParentTransactionsMapFromUtxos = async (UtxoFilter, db) => {
-    return (
-        (await db.Transactions.findAll({hash: {$in: UtxoFilter}}))
-        .reduce((acc, Transaction) => {
-            acc[Transaction.hash] = Transaction
-            return acc
-        }, {})
-    )
-}
+  return (await db.Transactions.findAll({ hash: { $in: UtxoFilter } })).reduce(
+    (acc, Transaction) => {
+      acc[Transaction.hash] = Transaction;
+      return acc;
+    },
+    {}
+  );
+};
 
 const createNewUtxoBodies = (vout, MappedTransactions, SpenderAccount) => {
-    return vout.map((utxo, index) => {
+  return vout.map((utxo, index) => {
+    return {
+      account: SpenderAccount.id,
+      transaction_id: MappedTransactions[utxo.hash]?.id ?? -1,
+      value_sats: toBigInt(utxo.value.toString(), 8),
+      hash: utxo.hash,
+      vout_index: index,
+      rune_balances: {},
+    };
+  });
+};
 
+const processEtching = async (InputAllocations, Transaction, db) => {
+  const { block, txIndex, runestone } = Transaction;
 
-        return {
-            account: SpenderAccount.id,
-            transaction_id: MappedTransactions[utxo.hash]?.id ?? -1,
-            value_sats: toBigInt(utxo.value.toString(), 8),
-            hash: utxo.hash,   
-            vout_index: index,
-            rune_balances: {}
-        }
-    })
-}
+  const { etching } = runestone;
 
-const processEtching = async (
-    InputAllocations, 
-    Transaction, 
-    db
-) => {
+  const { Rune } = db;
 
-    const {
-        block, 
-        txIndex,
-        runestone
-    } = Transaction;
+  console.log(etching);
 
-    const {
-        etching
-    } = runestone
+  //If no etching, return the input allocations
+  if (!runestone.etching) {
+    return InputAllocations;
+  }
+  console.log(etching.rune);
+  //If rune name already taken, it is non standard, return the input allocations
 
-    const {
-        Rune
-    } = db;
+  //Check if a rune name was provided, and if not, generate one
+  let runeName = etching.rune ?? getReservedName(block, txIndex);
 
-    //If no etching, return the input allocations
-    if(!runestone.etching){ return InputAllocations; }
-    console.log(etching.rune.name)
-    //If rune name already taken, it is non standard, return the input allocations
+  let spacedRune;
 
-    const isRuneNameTaken = await Rune.findOne({where: {name: etching.rune.name}})
+  if (etching.spacers) {
+    spacedRune = new SpacedRune(OrdRune.fromString(runeName), etching.spacers);
+  }
 
-    if(isRuneNameTaken) { return InputAllocations; }
-    
-    //Check if a rune name was provided, and if not, generate one
-    let runeName = etching.rune.name ?? getReservedName(block, txIndex)
+  const isRuneNameTaken = await Rune.findOne({
+    where: { name: spacedRune.name },
+  });
 
-    let spacedRune;
+  if (isRuneNameTaken) {
+    return InputAllocations;
+  }
 
-    if(etching.spacers){ 
-        spacedRune = new SpacedRune(OrdRune.fromString(runeName), etching.spacers);
-    }
+  const EtchedRune = await Rune.create({
+    rune_protocol_id: `${block}:${txIndex}`,
+    name: spacedRune ? spacedRune.name : runeName,
+    raw_name: runeName,
+    symbol: etching.symbol ?? "¤",
+    spacers: etching.spacers ?? 0,
 
-    const EtchedRune = await Rune.create({
-        rune_protocol_id: `${block}:${txIndex}`,
-        name: spacedRune ? spacedRune.name : runeName,
-        raw_name: runeName,
-        symbol: etching.symbol ?? '¤',
-        spacers: etching.spacers ?? 0,
+    //ORD describes no decimals being set as default 0
+    decimals: etching.divisibility ?? 0,
 
-        //ORD describes no decimals being set as default 0
-        decimals: etching.divisibility ?? 0,
+    total_supply: 0, //This is updated on transfer edict
+    total_holders: 0, //This is updated on transfer edict
 
-    
-        total_supply: 0, //This is updated on transfer edict
-        total_holders: 0,//This is updated on transfer edict
+    premine: etching.premine ?? "0",
 
-        premine: etching.premine ?? '0',
-
-        /*
+    /*
 
             ORD chooses the greater of the two values for mint start (height, offset)
             and the lesser of two values for mint_end (height, offset)
@@ -239,85 +207,82 @@ const processEtching = async (
             This is implemented in isMintOpen function
         */
 
-        mint_cap: etching.terms?.cap ?? null, // null for no cap, otherwise the cap
-        mint_amount: etching.terms?.amount ?? null,
-        mint_start: etching.terms?.height?.start ?? null,
-        mint_end: etching.terms?.height?.end ?? null,
-        mint_offset_start: etching.terms?.offset?.start ?? null,
-        mint_offset_end: etching.terms?.offset?.end ?? null,
-        turbo: etching.turbo
-    })
+    mint_cap: etching.terms?.cap ?? null, // null for no cap, otherwise the cap
+    mint_amount: etching.terms?.amount ?? null,
+    mint_start: etching.terms?.height?.[0] ?? null,
+    mint_end: etching.terms?.height?.[1] ?? null,
+    mint_offset_start: etching.terms?.offset?.[0] ?? null,
+    mint_offset_end: etching.terms?.offset?.[1] ?? null,
+    turbo: etching.turbo,
+  });
 
-    //Add premine runes to input allocations
-    return {
-        ...InputAllocations,
-        [EtchedRune.rune_protocol_id]: BigInt(etching.premine ?? '0')
-    }
+  //Add premine runes to input allocations
+  return {
+    ...InputAllocations,
+    [EtchedRune.rune_protocol_id]: BigInt(etching.premine ?? "0"),
+  };
+};
 
-}
+const processRunestone = async (Transaction, db) => {
+  const { runestone, hash, vout, vin } = Transaction;
 
-const processRunestone = async(Transaction, db) => {
+  const {
+    Account,
+    Balance,
+    Rune,
+    Runestone: RunestoneModel,
+    Transactions,
+    Utxo,
+  } = db;
 
-    const {
-        runestone,
-        hash,
-        vout,
-        vin
-    } = Transaction;
+  // const SpenderAccount = await _getAccount(Transaction, db)
 
-    const {
-        Account,
-        Balance,
-        Rune,
-        Runestone: RunestoneModel,
-        Transactions,
-        Utxo
-    } = db;
+  let UtxoFilter = vin.map((vin) => vin.txid);
 
-   // const SpenderAccount = await _getAccount(Transaction, db)
+  //Setup Transaction for processing
+  let InputUtxos = await Utxo.findAll({
+    where: { hash: { [Op.in]: UtxoFilter } },
+  });
+  let SpenderAccount = await getAccount(
+    InputUtxos[0]?.address ?? "GENESIS",
+    db
+  );
+  let RuneAllocations = getRuneAllocationsFromUtxos(InputUtxos);
+  //let MappedTransactions = await getParentTransactionsMapFromUtxos(UtxoFilter, db)
 
-    let UtxoFilter = vin.map(vin => vin.txid)
+  //Create A New Transaction to store UTXOs
+  let NewTransaction = await createTransaction(Transaction, SpenderAccount, db);
 
-    //Setup Transaction for processing
-    let InputUtxos = await Utxo.findAll({where: {hash: { [Op.in]: UtxoFilter } }})
-    let SpenderAccount = await getAccount(InputUtxos[0]?.address ?? 'GENESIS', db)
-    let RuneAllocations = getRuneAllocationsFromUtxos(InputUtxos)
-    //let MappedTransactions = await getParentTransactionsMapFromUtxos(UtxoFilter, db)
+  let NewUtxos = createNewUtxoBodies(vout, "", SpenderAccount);
 
-    //Create A New Transaction to store UTXOs
-    let NewTransaction = await createTransaction(Transaction, SpenderAccount, db)
+  //Delete UTXOs as they are being spent
+  // => This should be processed at the end of the block, with filters concatenated.. await Utxo.deleteMany({hash: {$in: UtxoFilter}})
 
-    let NewUtxos = createNewUtxoBodies(vout, '', SpenderAccount)
+  RuneAllocations = await processEtching(RuneAllocations, Transaction, db);
 
+  //Mints are processed next and added to the RuneAllocations, with caps being updated (and burnt in case of cenotaphs)
 
-    //Delete UTXOs as they are being spent
-    // => This should be processed at the end of the block, with filters concatenated.. await Utxo.deleteMany({hash: {$in: UtxoFilter}})
+  //TODO: Add mints as described abover
 
+  console.log(RuneAllocations);
+};
 
-    RuneAllocations = await processEtching(RuneAllocations, Transaction, db)
-    
-    //Mints are processed next and added to the RuneAllocations, with caps being updated (and burnt in case of cenotaphs)
+const testEdictRune = JSON.parse(
+  fs.readFileSync(
+    path.join(__dirname, "../../dumps/testEdictRune.json"),
+    "UTF-8"
+  )
+);
 
+const test = async () => {
+  const db = await databaseConnection();
 
-    //TODO: Add mints as described abover
+  //const rune = await db.Rune.findOne({where: {name: 'FIAT•IS•HELL•MONEY'}})
+  //console.log(isMintOpen(844000, rune))
 
-    console.log(RuneAllocations)
-    
-}
+  processRunestone(testEdictRune, db);
+};
 
-const testEdictRune = JSON.parse(fs.readFileSync(path.join(__dirname, '../../dumps/testEdictRune.json'), 'UTF-8'))
-
-
-const test  = async () => {
-    const db = await databaseConnection()
-
-    
-    const rune = await db.Rune.findOne({where: {name: 'FIAT•IS•HELL•MONEY'}})
-    console.log(isMintOpen(844000, rune))
-
-    //processRunestone(testEdictRune, db)
-}
-
-test()
+test();
 
 module.exports = processRunestone;
