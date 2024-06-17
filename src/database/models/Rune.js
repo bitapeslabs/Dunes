a9ce5501a1a7bff176b8982e7703dec350050737
encloinc <jelch2002@gmail.com>
2024-06-17 14:01:42 -0500

    feat(RAI-3/indexer.js,runeutils.js,Rune.js): Implement mints, fix mint-cap logic

--- src/database/models/Rune.js
+++ src/database/models/Rune.js
@@ -1,76 +1,88 @@
 const { Sequelize } = require("sequelize");
 
 module.exports = (sequelize) => {
-    return sequelize.define('Rune', {
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        rune_protocol_id: {
-            type: Sequelize.TEXT('tiny')	,
-            allowNull: false
-        },
-        name: {
-            type: Sequelize.TEXT('tiny')	,
-            allowNull: false
-        },
-        raw_name: {
-            type: Sequelize.TEXT('tiny')	,
-            allowNull: false
-        },
-        symbol: {
-            type: Sequelize.STRING(1),
-            allowNull: false
-        },
-        spacers: {
-            type: Sequelize.INTEGER,
-            allowNull: false
-        },
-        total_supply: {
-            type: Sequelize.INTEGER,
-            allowNull: false
-        },
-        decimals: {
-            type: Sequelize.INTEGER,
-            allowNull: false
-        },
-        premine: {
-            type: Sequelize.TEXT('tiny'),
-            allowNull: false
-        },
-        total_holders: {
-            type: Sequelize.INTEGER,
-            allowNull: false
-        },
-        mint_cap: {
-            type: Sequelize.TEXT('tiny'),
-            allowNull: true
-        },
-        mint_start: {
-            type: Sequelize.INTEGER,
-            allowNull: true
-        },
-        mint_end: {
-            type: Sequelize.INTEGER,
-            allowNull: true
-        },
-        mint_offset_start: {
-            type: Sequelize.INTEGER,
-            allowNull: true
-        },
-        mint_offset_end: {
-            type: Sequelize.INTEGER,
-            allowNull: true
-        },
-        mint_amount: {
-            type: Sequelize.TEXT('tiny'),
-            allowNull: true
-        }
-    }, {
-        tableName: 'runes',
-        timestamps: true,
-        createdAt: true,
-        updatedAt: true
-    });
-}
+  return sequelize.define(
+    "Rune",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      rune_protocol_id: {
+        type: Sequelize.TEXT("tiny"),
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.TEXT("tiny"),
+        allowNull: false,
+      },
+      raw_name: {
+        type: Sequelize.TEXT("tiny"),
+        allowNull: false,
+      },
+      symbol: {
+        type: Sequelize.STRING(1),
+        allowNull: false,
+      },
+      spacers: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      total_supply: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      decimals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      premine: {
+        type: Sequelize.TEXT("tiny"),
+        allowNull: false,
+      },
+      total_holders: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      mints: {
+        type: Sequelize.TEXT("tiny"),
+        allowNull: false,
+      },
+      mint_cap: {
+        type: Sequelize.TEXT("tiny"),
+        allowNull: true,
+      },
+      mint_start: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      mint_end: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      mint_offset_start: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      mint_offset_end: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      mint_amount: {
+        type: Sequelize.TEXT("tiny"),
+        allowNull: true,
+      },
+      unmintable: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "runes",
+      timestamps: true,
+      createdAt: true,
+      updatedAt: true,
+    }
+  );
+};
--- src/lib/runeutils.js
+++ src/lib/runeutils.js
@@ -1,57 +1,82 @@
+const getReservedName = (block, tx) => {
+  const baseValue = BigInt("6402364363415443603228541259936211926");
+  const combinedValue = (BigInt(block) << 32n) | BigInt(tx);
+  return baseValue + combinedValue;
+};
 
-const getReservedName = (block, tx)  =>  {
-    const baseValue = BigInt("6402364363415443603228541259936211926");
-    const combinedValue = (BigInt(block) << 32n) | BigInt(tx);
-    return baseValue + combinedValue;
-}
+const updateAllocations = (prevAllocations, Allocation) => {
+  /*
+    An "Allocation" looks like the following:
+    {
+        rune_id: string,
+        amount: BigInt
+    }
+  */
 
-const isMintOpen = (
-    
-    block,
-    Rune, 
-    mint_offset = BigInt(0)
+  prevAllocations[Allocation.rune_id] =
+    BigInt(prevAllocations[Allocation.rune_id] ?? "0") + Allocation.amount;
+  return prevAllocations;
+};
 
-) => {
+const isMintOpen = (block, Rune, mint_offset = false) => {
+  /*
+    if mint_offset is false, this function uses the current supply for calculation. If mint_offset is true,
+    the total_supply + mint_amount is used (it is used to calculate if a mint WOULD be allowed)
+  */
 
-    /*
-        Mint offset is given as a BigInt, it is a function used to check if minting would be allowed if x
-        amount of Rune was minted
-    */
+  let {
+    mints,
+    mint_cap,
+    mint_start,
+    mint_end,
+    mint_amount,
+    mint_offset_start,
+    mint_offset_end,
+    rune_protocol_id,
+    unmintable,
+  } = Rune;
 
-    let {
-        mint_cap,
-        mint_start,
-        mint_end,
-        mint_offset_start,
-        mint_offset_end,
-        total_supply,
-        rune_protocol_id
+  if (unmintable) {
+    return false;
+  } //If the rune is unmintable, minting is globally not allowed
 
-    } = Rune;
-    
-    let [
-        creationBlock, 
-    ] = rune_protocol_id.split(':').map(parseInt)
- 
-    /*
+  let [creationBlock] = rune_protocol_id.split(":").map(parseInt);
+
+  /*
         Setup variable defs according to ord spec,
     */
 
-    mint_cap = BigInt(mint_cap) ?? BigInt(0) //If no mint cap is provided, minting is by default closed so we default to 0 which will negate any comparisons
-    
-    //Convert offsets to real block heights
-    mint_offset_start = (mint_offset_start ?? 0) + creationBlock
-    mint_offset_end = (mint_offset_end ?? 0) + creationBlock
+  mint_cap = BigInt(mint_cap) ?? BigInt(0); //If no mint cap is provided, minting is by default closed so we default to 0 which will negate any comparisons
 
-    //If the mint offset (amount being minted) causes the total supply to exceed the mint cap, this mint is not allowed
-    if((BigInt(total_supply) + mint_offset) >= mint_cap){ return false; }
-    
-    //Define defaults used for calculations below
-    const starts = [mint_start, mint_offset_start].filter(e => e !== creationBlock)
-    const ends = [mint_end, mint_offset_end].filter(e => e !== creationBlock)
+  //Convert offsets to real block heights
+  mint_offset_start = (mint_offset_start ?? 0) + creationBlock;
+  mint_offset_end = (mint_offset_end ?? 0) + creationBlock;
 
+  /*
 
-    /*
+  mint_cap and premine are separate. See
+    https://github.com/ordinals/ord/blob/6103de9780e0274cf5010f3865f0e34cb1564b58/src/index/entry.rs#L60
+  line 95 
+  
+  for this reason when calculating if a Rune has reached its mint cap, we must first remove the premine from the total supply to get
+  the actual runes generated from mints alone.
+  */
+
+  //This should always be perfectly divisible, since mint_amount is the only amount always added to the total supply
+  total_mints = BigInt(mints) + BigInt(mint_offset ? mint_amount : "0");
+
+  //If the mint offset (amount being minted) causes the total supply to exceed the mint cap, this mint is not allowed
+  if (total_mints >= mint_cap) {
+    return false;
+  }
+
+  //Define defaults used for calculations below
+  const starts = [mint_start, mint_offset_start].filter(
+    (e) => e !== creationBlock
+  );
+  const ends = [mint_end, mint_offset_end].filter((e) => e !== creationBlock);
+
+  /*
         If both values differ from the creation block, it can be assumed that they were both provided during etching.
         In this case, we want to find the MAXIMUM value according to ord spec.
 
@@ -62,35 +87,29 @@ const isMintOpen = (
         If no values are provided, start is the creationBlock.
 
     */
-    const start = (starts.length === 2) ? Math.max(
-        (mint_start ?? creationBlock),
-        mint_offset_start
-    ) : (starts[0] ?? creationBlock)
-   
+  const start =
+    starts.length === 2
+      ? Math.max(mint_start ?? creationBlock, mint_offset_start)
+      : starts[0] ?? creationBlock;
 
-    /*
+  /*
 
         Same as start with a few key differences: we use the MINIMUM value for the ends. If one is provided we use that one and if not are provided
         block is set to Infinity to allow minting to continue indefinitely.
     */
 
-    const end = (ends.length === 2) ? Math.min(
-        (mint_end ?? mint_offset_end),
-        mint_offset_end 
-    ) : (ends[0] ?? Infinity)
+  const end =
+    ends.length === 2
+      ? Math.min(mint_end ?? mint_offset_end, mint_offset_end)
+      : ends[0] ?? Infinity;
 
+  //Perform comparisons
 
-    console.log(start)
-    console.log(end)
-
-
-    //Perform comparisons    
-
-    return !(start > block || end < block);
-}
+  return !(start > block || end < block);
+};
 
 module.exports = {
-    getReservedName,
-    isMintOpen
-}
-
+  getReservedName,
+  isMintOpen,
+  updateAllocations,
+};
