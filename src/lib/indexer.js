3cc3d5091f53ea048f8b070acd66bd0535a04adc
encloinc <jelch2002@gmail.com>
2024-07-17 17:47:34 -0500

    debug

--- src/lib/indexer.js
+++ src/lib/indexer.js
@@ -645,6 +645,26 @@ const loadBlockIntoMemory = async (block, storage) => {
 };
 
 const processBlock = async (blockHeight, callRpc, storage) => {
+  const formatMemoryUsage = (data) =>
+    `${Math.round((data / 1024 / 1024) * 100) / 100} MB`;
+
+  const memoryData = process.memoryUsage();
+
+  const memoryUsage = {
+    rss: `${formatMemoryUsage(
+      memoryData.rss
+    )} -> Resident Set Size - total memory allocated for the process execution`,
+    heapTotal: `${formatMemoryUsage(
+      memoryData.heapTotal
+    )} -> total size of the allocated heap`,
+    heapUsed: `${formatMemoryUsage(
+      memoryData.heapUsed
+    )} -> actual memory used during the execution`,
+    external: `${formatMemoryUsage(memoryData.external)} -> V8 external memory`,
+  };
+
+  log(JSON.stringify(memoryUsage), "debug");
+
   const blockData = await getRunestonesInBlock(blockHeight, callRpc);
 
   //Load all rows we will manipulate beforehand into memory
